f67c1e21730b8f31ecedb29f194004423fc9061f
yusufboyaci <boyacyusuf@gamil.com>
2023-02-03 15:26:04 +0300

    Add project files.

---
+++ NTierProject1B.CORE/Enum/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierProject1B.CORE.Enum
+{
+    public enum Status
+    {
+        None = 1,
+        Active,
+        Updated,
+        Deleted
+    }
+}
